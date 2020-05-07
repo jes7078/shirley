ce5a9b84bbbfbf61d85dfaad23c8bbc201f4f882
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-05-07 13:50:36 -0400

    first

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import '../components/fonts.scss'
 
 const HomePage = () => {
-  return <HelloWorld />
+  return (
+    <div>
+      <section className="fontSection">
+        <h1 className="marck, deepSeaSpace">Shirley</h1>
+        <h2 className="marck, deepSeaSpace">
+          font-family: 'Marck Script', cursive
+        </h2>
+        <h2 className="marck, deepSeaSpace">Gradient: deepSeaSpace</h2>
+      </section>
+      <section className="fontSection">
+        <h1 className="pinyon, deepSeaSpace">Shirley</h1>
+        <h2 className="pinyon, deepSeaSpace">
+          font-family: 'Marck Script', cursive
+        </h2>
+        <h2 className="pinyon, deepSeaSpace">Gradient: deepSeaSpace</h2>
+      </section>
+    </div>
+  )
 }
 
 export default HomePage
